3d3dc0681a4f34d8d1539c23256ee172f5486f4d
amtoaer <amtoaer@outlook.com>
2020-07-29 11:46:56 +0800

    泛型中擦拭法的理解

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,59 +1,46 @@
 package com.company;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.Class;
 
 public class Main {
   public static void main(String[] args) {
-    var person = new Person("amtoaer", "Northeastern University,Shenyang,Liaoning,China");
+    Pair<String> test1 = new Pair<>("Hello", "world");
+    Pair<Integer> test2 = new Pair<>(123, 456);
+    // 无法取得带泛型的class，该式两边均为Pair.class
+    System.out.println(test1.getClass() == test2.getClass());
+    // 无法使用该式判断带泛型的类型（编译报错）
+    System.out.println(test2 instanceof Pair<String>);
+    // 正确实例化T类型的方式
     try {
-      check(person);
-      person.printInfo();
-    } catch (IllegalAccessException | IllegalArgumentException e) {
+      Pair<String> test3 = new Pair<>(String.class);
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
+}
 
-  static void check(Person person) throws IllegalArgumentException, IllegalAccessException {
-    for (var field : Person.class.getDeclaredFields()) {
-      field.setAccessible(true);
-      var range = field.getAnnotation(Range.class);
-      if (range != null) {
-        Object value = field.get(person);
-        if (value instanceof String tmp) {
-          if (tmp.length() < range.min() || tmp.length() > range.max()) {
-            throw new IllegalArgumentException(
-                "Illegal field \"" + field.getName() + "\". Please check the length of String");
-          }
-        }
-      }
-    }
+class Pair<T> {
+  private final T first;
+  private final T second;
+
+  public Pair(T first, T second) {
+    this.first = first;
+    this.second = second;
+  }
+
+  // 这是错误的实例化T的做法（因为擦拭法）
+  public Pair() {
+    this.first = new T();
+    this.second = new T();
+  }
+
+  // 如果需要实例化，需要显式传入Class<T>参数：
+  public Pair(Class<T> c) throws Exception {
+    first = c.getDeclaredConstructor().newInstance();
+    second = c.getDeclaredConstructor().newInstance();
   }
 }
 
-@Target(ElementType.FIELD)
-@Retention(RetentionPolicy.RUNTIME)
-@interface Range {
-  int min() default 0;
-
-  int max() default 255;
-}
-
-class Person {
-  @Range(min = 1, max = 20)
-  private final String name;
-
-  @Range(max = 10)
-  private final String school;
-
-  Person(String name, String school) {
-    this.name = name;
-    this.school = school;
-  }
-
-  void printInfo() {
-    System.out.println("My name is " + this.name + ", studying at " + school);
-  }
+// 一个类可以继承自一个泛型类，例如：
+class IntPair extends Pair<Integer> {
 }
