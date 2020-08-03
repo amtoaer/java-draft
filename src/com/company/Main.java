40a4c604302a874d65d9353b20ab78a160142c91
amtoaer <amtoaer@outlook.com>
2020-08-03 21:14:01 +0800

    通过set对list去重

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,34 @@
 package com.company;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    Map<Person, Integer> map = new TreeMap<>();
-    map.put(new Person("Ali", 100), 1);
-    map.put(new Person("Kitty", 90), 2);
-    map.put(new Person("Bily", 80), 3);
-    for (var entry : map.entrySet()) {
-      System.out.print(entry.getKey().getName() + "(" + entry.getKey().getScore() + ")");
-      System.out.println(" : " + entry.getValue());
+    List<Message> received = List.of(new Message(1, "Hello!"), new Message(2, "发工资了吗？"), new Message(2, "发工资了吗？"),
+        new Message(3, "去哪吃饭？"), new Message(3, "去哪吃饭？"), new Message(4, "Bye"));
+    List<Message> displayMessages = process(received);
+    for (Message message : displayMessages) {
+      System.out.println(message.text);
     }
-    /*
-     * 因为SortedMap.get()内部通过CompareTo判断相等，所以CompareTo需要严谨的实现
-     * 以当前Person中使用的CompareTo为例，因为只考虑到了通过Person.name进行排序
-     * 导致在通过Key进行map.get()时，只需要key的name字段相同即可获取到value
-     */
-    System.out.println(map.get(new Person("Ali", 80)));
+  }
+
+  static List<Message> process(List<Message> received) {
+    Set<Message> set = new TreeSet<>(received);
+    return List.copyOf(set);
   }
 }
 
-class Person implements Comparable<Person> {
-  private final String name;
-  private final int score;
+class Message implements Comparable<Message> {
+  public final int sequence;
+  public final String text;
 
-  public Person(String name, int score) {
-    this.name = name;
-    this.score = score;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public int getScore() {
-    return this.score;
+  public Message(int sequence, String text) {
+    this.sequence = sequence;
+    this.text = text;
   }
 
   @Override
-  public int compareTo(Person others) {
-    return this.name.compareTo(others.name);
+  public int compareTo(Message o) {
+    return this.sequence - o.sequence;
   }
 }
