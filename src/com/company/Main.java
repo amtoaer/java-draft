3f954f43f34ee5f7401d410fcb80ccc9b79b76b4
amtoaer <amtoaer@outlook.com>
2020-08-04 21:19:54 +0800

    实现递归打印目录

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,36 @@
 package com.company;
 
-import java.util.*;
+import java.io.*;
 
 public class Main {
-  public static void main(String[] args) {
-    String hex = toHex(12500);
-    if (hex.equalsIgnoreCase("30D4")) {
-      System.out.println("测试通过");
+  public static void main(String[] args) throws Exception {
+    File f;
+    if (args.length == 0) {
+      f = new File("./").getCanonicalFile();
+    } else if (args.length == 1) {
+      f = new File(args[0]).getCanonicalFile();
     } else {
-      System.out.println("测试失败");
+      throw new Exception("len(args) must be 0 or 1");
+    }
+    if (f.exists()) {
+      if (f.isDirectory()) {
+        list(f, "");
+      } else {
+        throw new IOException("args[0] is not a directory");
+      }
+    } else {
+      throw new IOException("directory doesn't exist");
     }
   }
 
-  public static String toHex(int n) {
-    Deque<String> stack = new LinkedList<>();
-    while (n != 0) {
-      stack.push(Integer.toHexString(n % 16));
-      n = n / 16;
+  public static void list(File file, String space) {
+    if (file.isDirectory()) {
+      System.out.println(space + file.getName() + File.separator);
+      for (var item : file.listFiles()) {
+        list(item, space + "  ");
+      }
+    } else if (file.isFile()) {
+      System.out.println(space + "  " + file.getName());
     }
-    String result = "";
-    while (!stack.isEmpty()) {
-      result += stack.pop();
-    }
-    return result;
   }
 }
