1d4212c6d418ee73fc18fa272d2d7bcdf95c95f9
amtoaer <amtoaer@outlook.com>
2020-09-04 19:33:40 +0800

    使用synchronized修饰方法加锁this

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,22 @@
 package com.company;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         var c = new Counter();
-        new Thread(() -> {
+        var a = new Thread(() -> {
             for (int i = 0; i < 100; i++) {
                 c.add(1);
             }
-        }).start();
-        new Thread(() -> {
+        });
+        var b = new Thread(() -> {
             for (int i = 0; i < 100; i++) {
                 c.dec(1);
             }
-        }).start();
+        });
+        a.start();
+        b.start();
+        a.join();
+        b.join();
         System.out.println(c.get());
     }
 }
@@ -20,16 +24,12 @@ public class Main {
 class Counter {
     private int count = 0;
 
-    public void add(int n) {
-        synchronized (this) {
-            count += n;
-        }
+    public synchronized void add(int n) {
+        count += n;
     }
 
-    public void dec(int n) {
-        synchronized (this) {
-            count -= n;
-        }
+    public synchronized void dec(int n) {
+        count -= n;
     }
 
     public int get() {
