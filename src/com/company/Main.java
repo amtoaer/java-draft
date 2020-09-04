654ce840ed1d29f22777f610542d3ef9c8f54ce4
amtoaer <amtoaer@outlook.com>
2020-09-04 18:12:42 +0800

    使用锁确保线程同步

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,17 +13,26 @@ public class Main {
 }
 
 class Counter {
+    public static Object lock = new Object();
     public static int count = 0;
 }
 
 class AddThread extends Thread {
     public void run() {
-        for (int i=0; i<10000; i++) { Counter.count += 1; }
+        synchronized (Counter.lock) {
+            for (int i = 0; i < 10000; i++) {
+                Counter.count += 1;
+            }
+        }
     }
 }
 
 class DecThread extends Thread {
     public void run() {
-        for (int i=0; i<10000; i++) { Counter.count -= 1; }
+        synchronized (Counter.lock) {
+            for (int i = 0; i < 10000; i++) {
+                Counter.count -= 1;
+            }
+        }
     }
 }
