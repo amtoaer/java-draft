6fc19d0bf6ae4d7a8fc4e01dd57bdf5a6553c95d
amtoaer <amtoaer@outlook.com>
2020-09-14 12:01:33 +0800

    java小作业

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,59 +1,35 @@
 package com.company;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Main {
-  public static void main(String[] args) {
-    var person = new Person("amtoaer", "Northeastern University,Shenyang,Liaoning,China");
-    try {
-      check(person);
-      person.printInfo();
-    } catch (IllegalAccessException | IllegalArgumentException e) {
-      e.printStackTrace();
-    }
-  }
-
-  static void check(Person person) throws IllegalArgumentException, IllegalAccessException {
-    for (var field : Person.class.getDeclaredFields()) {
-      field.setAccessible(true);
-      var range = field.getAnnotation(Range.class);
-      if (range != null) {
-        Object value = field.get(person);
-        if (value instanceof String tmp) {
-          if (tmp.length() < range.min() || tmp.length() > range.max()) {
-            throw new IllegalArgumentException(
-                "Illegal field \"" + field.getName() + "\". Please check the length of String");
-          }
+class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        String target;
+        try (var scanner = new Scanner(System.in)) {
+            System.out.println("Please input target String:");
+            target = scanner.nextLine();
+            System.out.println("Please input String list (end with 0)");
+            String tmp;
+            while (!"0".equals(tmp = scanner.nextLine())) {
+                list.add(tmp);
+            }
         }
-      }
+        System.out.print("result is: ");
+        System.out.println(find(list, target));
     }
-  }
-}
 
-@Target(ElementType.FIELD)
-@Retention(RetentionPolicy.RUNTIME)
-@interface Range {
-  int min() default 0;
-
-  int max() default 255;
-}
-
-class Person {
-  @Range(min = 1, max = 20)
-  private final String name;
-
-  @Range(max = 10)
-  private final String school;
-
-  Person(String name, String school) {
-    this.name = name;
-    this.school = school;
-  }
-
-  void printInfo() {
-    System.out.println("My name is " + this.name + ", studying at " + school);
-  }
+    private static int find(List<String> list, String target) {
+        if (target == null) {
+            return -1;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (target.equals(list.get(i))) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
