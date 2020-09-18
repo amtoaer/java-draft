8fef2b8010006fa82ca7a2afa7d030add34698a9
amtoaer <amtoaer@outlook.com>
2020-09-18 22:47:55 +0800

    查找相同字符串的字典树实现

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,135 +1,80 @@
 package com.company;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Map;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        // 输入的待查找的字符串
-        List<String> list = new ArrayList<>();
-        String target;
-        try (var scanner = new Scanner(System.in)) {
-            System.out.print("Please input target String:");
-            // 目标字符串
-            target = scanner.nextLine();
-            System.out.println("Please input String array to find(end with space):");
-            String tmp;
-            // 输入非空则读入list
-            while (!"".equals(tmp = scanner.nextLine())) {
-                list.add(tmp);
-            }
+        String[] toFind = new String[] { "abca", "abab", "abba", "abca" };
+        String target = "abca";
+        TrieTreeNode root = new TrieTreeNode();
+        for (int i = 0; i < toFind.length; i++) {
+            root.addStringToTree(toFind[i], i);
         }
-        // 输出查找结果
-        System.out.println(find(target, getMap(list)));
-
-    }
-
-    // 字符串分割函数
-    private static List<String> seprateString(String src, int size) {
-        List<String> result = new ArrayList<>();
-        int begin = 0;
-        while (begin < src.length()) {
-            // 如果开始位置加截断长度大于源字符串长度，则只截断到字符串末尾并跳出循环
-            if (begin + size > src.length()) {
-                result.add(src.substring(begin));
-                break;
-            }
-            result.add(src.substring(begin, begin + size));
-            begin += size;
-        }
-        return result;
-    }
-
-    private static Map<String, List<Pair>> getMap(List<String> list) {
-        Map<String, List<Pair>> map = new HashMap<>();
-        for (int i = 0; i < list.size(); i++) {
-            String item = list.get(i);
-            // 对待查找字符串进行分割得到List<String>
-            var array = seprateString(item, 10);
-            // 遍历所有分割结果
-            for (int j = 0; j < array.size(); j++) {
-                String key = array.get(j);
-                // 将分割结果作为key对map取value,如果为空则加入空List
-                if (map.get(key) == null) {
-                    map.put(key, new ArrayList<Pair>());
-                }
-                // 为List加入二元组<i,j>（其中i为待查找字符串的编号，j为在该字符串内的位置）
-                map.get(key).add(new Pair(i, j));
-            }
-        }
-        return map;
-    }
-
-    private static Set<Integer> find(String target, Map<String, List<Pair>> map) {
-        // target为空则返回空集合
-        if (target == null) {
-            return new HashSet<Integer>();
-        }
-        List<Set<Integer>> list = new ArrayList<>();
-        // 对需查找的字符串进行分割
-        var array = seprateString(target, 10);
-        // 遍历分割后的字符串数组
-        for (int i = 0; i < array.size(); i++) {
-            var key = array.get(i);
-            // 与该分割段在对应位置相等的行数集合
-            Set<Integer> set = new HashSet<>();
-            // 如果该段匹配，则将行数加入到set中
-            for (var item : map.get(key)) {
-                if (item.getLast() == i) {
-                    set.add(item.getFirst());
-                }
-            }
-            // 将set加入list
-            list.add(set);
-        }
-        // 如果list为空则返回空集合
-        if (list.isEmpty()) {
-            return new HashSet<Integer>();
-        }
-        // 取list中的第一个集合
-        Set<Integer> result = list.get(0);
-        // 将该集合对所有集合取交
-        for (var item : list) {
-            result.retainAll(item);
-        }
-        // 得到结果，返回
-        return result;
+        System.out.println(root.searchInTree(target));
     }
 }
 
-// 一个简单的Pair实现（<Integer,Integer>）
-class Pair {
-    private final Integer first;
-    private final Integer last;
+class TrieTreeNode {
+    // 子节点
+    private List<TrieTreeNode> list;
+    // 存储角标
+    private List<Integer> result;
+    // 该点字符
+    private char ch;
 
-    public Pair(Integer first, Integer last) {
-        this.first = first;
-        this.last = last;
+    // 空构造函数(构造根节点)
+    public TrieTreeNode() {
     }
 
-    public Integer getFirst() {
-        return this.first;
+    // 构造子节点
+    private TrieTreeNode(char ch) {
+        this.ch = ch;
     }
 
-    public Integer getLast() {
-        return this.last;
-    }
-
-    @Override
-    public boolean equals(Object compare) {
-        if (compare == null) {
-            return false;
-        }
-        Pair tmp = (Pair) compare;
-        if (this.first == tmp.first && this.last == tmp.last) {
+    // 判断该点字符是否为ch
+    private boolean isChar(char ch) {
+        if (this.ch == ch) {
             return true;
         }
         return false;
     }
 
+    // 获取字符为ch的子节点(如果没有字符为ch的子节点则自动添加)
+    private TrieTreeNode getNext(char ch) {
+        if (list == null) {
+            list = new ArrayList<>();
+        }
+        for (var item : list) {
+            if (item.isChar(ch)) {
+                return item;
+            }
+        }
+        var tmp = new TrieTreeNode(ch);
+        list.add(tmp);
+        return tmp;
+    }
+
+    // 使用上述private函数将字符串添加到字典树中
+    public void addStringToTree(String str, int index) {
+        var tmp = this;
+        for (var item : str.toCharArray()) {
+            tmp = tmp.getNext(item);
+        }
+        if (tmp.result == null) {
+            tmp.result = new ArrayList<>();
+        }
+        tmp.result.add(index);
+    }
+
+    // 在字典树中查询目标字符串
+    public List<Integer> searchInTree(String target) {
+        var tmp = this;
+        for (var item : target.toCharArray()) {
+            if (tmp.list == null || tmp.list.isEmpty()) {
+                return new ArrayList<Integer>();
+            }
+            tmp = tmp.getNext(item);
+        }
+        return tmp.result;
+    }
 }
